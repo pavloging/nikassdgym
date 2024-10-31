674c8bb75db0f0006899eea5b576e59f20672d70
Egor Pavlov <ep9821123@gmail.com>
2024-10-31 12:43:34 +0300

    feat: update price

--- client/src/constants/subscription.ts
+++ client/src/constants/subscription.ts
@@ -5,13 +5,13 @@ export const subscription: Array<ISubscription> = [
         name: '24 часа',
         description: 'Разовый доступ на 24 часа',
         date: 24 * 60 * 60 * 1000, // 24 часа
-        price: 499
+        price: 599
     },
     {
         name: '1 месяц',
         description: 'Доступ на 30 дней',
         date: 30 * 24 * 60 * 60 * 1000, // 30 дней
-        price: 5999
+        price: 6999
     },
     {
         name: '3 месяца',
