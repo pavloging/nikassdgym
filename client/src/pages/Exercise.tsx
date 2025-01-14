6c4f4ec5d6f5e3a3defc8433f3165ff7c4f789f4
Egor <ep9821123@gmail.com>
2025-01-14 20:49:31 +0300

    feat: new logic for first-users

--- client/src/pages/Exercise.tsx
+++ client/src/pages/Exercise.tsx
@@ -1,29 +1,21 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import ContentContainer from '../components/ContentContainer';
 import { exercises as exercisesList } from '../constants/exercises';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import LazyLoadVideo from '../components/LazyLoadVideo';
 import { RootState } from '../redux/store';
 import { useSelector } from 'react-redux';
 
 const Exercise: FC = () => {
-    const store = useSelector((state: RootState) => state.user);
     const { name } = useParams();
-    const navigate = useNavigate();
     const [search, setSearch] = useState('');
-
-    if (!store.isAuth) return <p className="txt-center">Авторизуйтесь в системе и активируйте подписку</p>;
-
-    if (!store.user.isActivatedSubscription) return <p className="txt-center">У вас не активна подписка</p>;
-
+    const store = useSelector((state: RootState) => state.user);
+    
     const exercise = exercisesList.find((item) => item.url === name);
 
-    // Validate Page
-    useEffect(() => {
-        if (!exercise) navigate('/404');
-    }, [exercise, name, navigate]);
-
-    if (!exercise) return <h1>Page 404</h1>;
+    if (!store.isAuth) return <p className="txt-center">Авторизуйтесь в системе и активируйте подписку</p>;
+    if (!store.user.isActivatedSubscription) return <p className="txt-center">У вас не активна подписка</p>;
+    if (!exercise) return <h1>Страница не найдена</h1>;
 
     const filteredExercise =
         search.length !== 0
