44e8e6882b47ce0360e1e13ba4ef4e60ba4b273f
Egor Pavlov <egorpavlov@MacBook-Air-Egor.local>
2025-04-14 23:45:03 +0300

    feat: updated links

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -222,62 +222,32 @@ const Main: FC = () => {
                     >
                         <SwiperSlide>
                             <div className="main__result-block">
-                                <img
-                                    className="main__result-img"
-                                    loading="lazy"
-                                    src={benefitsNotRobot}
-                                    alt=""
-                                />
+                                <img className="main__result-img" src={benefitsNotRobot} alt="" />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="main__result-block">
-                                <img
-                                    className="main__result-img"
-                                    loading="lazy"
-                                    src={benefitsSystem}
-                                    alt=""
-                                />
+                                <img className="main__result-img" src={benefitsSystem} alt="" />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="main__result-block">
-                                <img
-                                    className="main__result-img"
-                                    loading="lazy"
-                                    src={benefitsConnect}
-                                    alt=""
-                                />
+                                <img className="main__result-img" src={benefitsConnect} alt="" />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="main__result-block">
-                                <img
-                                    className="main__result-img"
-                                    loading="lazy"
-                                    src={benefitsResult}
-                                    alt=""
-                                />
+                                <img className="main__result-img" src={benefitsResult} alt="" />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="main__result-block">
-                                <img
-                                    className="main__result-img"
-                                    loading="lazy"
-                                    src={benefitsForYou}
-                                    alt=""
-                                />
+                                <img className="main__result-img" src={benefitsForYou} alt="" />
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className="main__result-block">
-                                <img
-                                    className="main__result-img"
-                                    loading="lazy"
-                                    src={benefitsEscort}
-                                    alt=""
-                                />
+                                <img className="main__result-img" src={benefitsEscort} alt="" />
                             </div>
                         </SwiperSlide>
                     </Swiper>
@@ -544,7 +514,11 @@ const Main: FC = () => {
                         </div>
                     </div>
                     <div className="main__block-link_btn">
-                        <a href='https://wa.me/79141636665?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9F%D0%B8%D1%88%D1%83%20%D0%B2%D0%B0%D0%BC%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0%2C%20%D0%BC%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D1%80%D0%B0%D0%B7%D0%B1%D0%BE%D1%80%20%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7%D0%BE%D0%B2%2C%20%D0%BF%D0%BE%D0%B4%D1%81%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5%2C%20%D0%BD%D0%B0%20%D0%BA%D0%B0%D0%BA%D1%83%D1%8E%20%D0%B4%D0%B0%D1%82%D1%83%20%D0%B5%D1%81%D1%82%D1%8C%20%D0%B1%D0%BB%D0%B8%D0%B6%D0%B0%D0%B9%D1%88%D0%B0%D1%8F%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C%3F' target='_blank' className="main__link_btn">
+                        <a
+                            href="https://wa.me/79141636665?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5!%20%D0%9F%D0%B8%D1%88%D1%83%20%D0%B2%D0%B0%D0%BC%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0%2C%20%D0%BC%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D1%80%D0%B0%D0%B7%D0%B1%D0%BE%D1%80%20%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7%D0%BE%D0%B2%2C%20%D0%BF%D0%BE%D0%B4%D1%81%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5%2C%20%D0%BD%D0%B0%20%D0%BA%D0%B0%D0%BA%D1%83%D1%8E%20%D0%B4%D0%B0%D1%82%D1%83%20%D0%B5%D1%81%D1%82%D1%8C%20%D0%B1%D0%BB%D0%B8%D0%B6%D0%B0%D0%B9%D1%88%D0%B0%D1%8F%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C%3F"
+                            target="_blank"
+                            className="main__link_btn"
+                        >
                             <span>Узнать стоимость</span>
                             <img src="/icons/arrow-right-top-black.svg" alt="" />
                         </a>
