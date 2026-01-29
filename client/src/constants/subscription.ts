087219b10794b771a1064dbb1909f4261e166f12
Egor Pavlov <ep9821123@gmail.com>
2026-01-29 10:46:07 +0300

    Update subscription.ts

--- client/src/constants/subscription.ts
+++ client/src/constants/subscription.ts
@@ -5,20 +5,20 @@ export const subscription: Array<ISubscription> = [
         name: '24 часа',
         description: 'Разовый доступ на 24 часа',
         date: 24 * 60 * 60 * 1000, // 24 часа
-        price: 799
+        price: 899
     },
     {
         name: '1 месяц',
         description: 'Доступ на 4 недели',
         date: 28 * 24 * 60 * 60 * 1000, // 28 дней
-        price: 9600,
-        salePrice: 6999
+        price: 10000,
+        salePrice: 7999
     },
     {
         name: '3 месяца',
         description: 'Доступ на 12 недель',
         date: 84 * 24 * 60 * 60 * 1000, // 84 дней
-        price: 21000,
-        salePrice: 18900
+        price: 28000,
+        salePrice: 19999
     }
 ];
